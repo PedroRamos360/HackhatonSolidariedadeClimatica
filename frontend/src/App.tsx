fe5b6cb6453f1995c6b9622df245e21a2b080da6
JordanoXavier <112017191+JordanoXavier@users.noreply.github.com>
2024-05-20 23:07:59 -0300

    use react router dom in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { LandingPage } from './pages/LandingPage';
 
 function App() {
   return (
-   <LandingPage /> 
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </Router>
   );
 }
 
